914718692f936dedf67c676d125b2dd4fd398ddb
sobolevn <mail@sobolevn.me>
2023-09-05 09:27:23 +0300

    Fix tests

--- test.sh
+++ test.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env sh
 
-# Installing test dependencies:
-pip3 install safety
+set -e
 
 # Running tests:
 pip3 --version
 git --version
-docker-compose --version
+docker compose --version
 dump-env --version
 disl --version
+
+# Installing test dependencies:
+pip3 install safety
 
 # Running safety:
 safety check
